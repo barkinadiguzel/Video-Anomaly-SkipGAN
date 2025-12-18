41360839658c324b4d7e32437822aff64f0fdf35
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-18 16:06:00 +0300

    Create utils_layers.py

---
+++ src/layers/utils_layers.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+
+def weights_init_dcgan(m):
+    classname = m.__class__.__name__
+
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+        if hasattr(m, 'bias') and m.bias is not None:
+            nn.init.constant_(m.bias.data, 0.0)
+
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0.0)
